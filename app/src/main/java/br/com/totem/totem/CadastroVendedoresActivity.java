fecdd747219dde8ad16a368525b6195ef630b2bc
renato <renasc.rpn@gmail.com>
2017-11-14 01:24:24 -0200

    Initial commit

---
+++ app/src/main/java/br/com/totem/totem/CadastroVendedoresActivity.java
@@ -0,0 +1,13 @@
+package br.com.totem.totem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroVendedoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_vendedores);
+    }
+}
